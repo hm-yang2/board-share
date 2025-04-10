7cc65f8dce21d8def536964862a655ab6b01c7d0
hm-yang2 <hanming.yang@wsa.com>
2025-04-10 10:02:45 +0800

    Update SecurityConfig.java

--- api/src/main/java/com/powerbi/api/config/SecurityConfig.java
+++ api/src/main/java/com/powerbi/api/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://wsasgsgpdigits", "https://wsasgsgpdigits"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://wsasgsgpdigits"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
